ab2318fd0cbaba8af66c888d9ad22f303064f78c
JasonKarau <Karauski@gmail.com>
2020-09-29 11:17:07 -0500

    updated

--- app.js
+++ app.js
@@ -3,58 +3,45 @@ const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
 const resetBtn = document.querySelector('.btn__reset');
 const overlay = document.getElementById('overlay');
-const screenTitle = overlay.querySelector('h2');
-const scoreBoard = document.getElementById('scoreboard');
+const ul = document.querySelector('#phrase ul');
 
 // Establish and set game resources
+
 let missed = 0;
-let correct = 0;
 const phrases = [
-  'wu tang clan',
-  'mobb deep',
-  'a tribe called quest',
-  'black moon',
-  'group home'
+  'Wu Tang Clan',
+  'Mobb Deep',
+  'A Tribe Called Quest',
+  'Black Moon',
+  'Group Home'
 ];
+
 // Configure start game
-resetBtn.addEventListener('click', (e) => {
-  if (e.target.textContent === 'Start Game') {
-    overlay.classList.add('hidden');
-  } else if (e.target.textContent === 'Play Again') {
-    document.location.reload(true);
-  }
+resetBtn.addEventListener('click', () => {
+    overlay.style.display = 'none';
 });
 
-// Get the phrases on display
+// Return random phrases and get them to display
+const getRandomPhraseAsArray = (arr) => {
+  let ranNum = Math.floor(Math.random() * arr.length);
+  let ranPhrase = arr[ranNum].split("");
+  return ranPhrase;
+}
 
-// function addPhraseToDisplay (arr) {
-//   function getRandomPhraseAsArray (arr) {
-//     const random = Math.floor(Math.random() * arr.length );
-//     const randomPhrase = arr[random];
-//     const phraseArray = randomPhrase.split('');
-//     return phraseArray;
-//   };
-//   const phraseArray = getRandomPhraseAsArray(phrases);
-//   for (let i = 0; i < phraseArray.length; i++) {
-//     const li = document.createElement('li');
-//     const ul = document.createElement('ul');
-//     li.textContent = phraseArray[i].toUpperCase();
-//     ul.appendChild(li);
-//     let phraseCharacter = phraseArray[i];
-//     if (phraseCharacter === ' ') {
-//       li.classList.add('space');
-//     } else {
-//       li.classList.add('letter');
-//     }
-//   }
-// }
-// addPhraseToDisplay(phrases);
-// const phraseLI = phrase.querySelectorAll('li.letter');
+getRandomPhraseAsArray(phrases);
 
-// Display heart score
+const addPhraseToDisplay = arr => {
+    for(i = 0; i < arr.length; i++) {
+        const li = document.createElement('li');
+        li.textContent = arr[i];
+        ul.appendChild(li);
+        if(arr[i] !== ' ') {
+            li.classList.add('letter');
+        } else {
+            li.classList.add('space');
+            }
+        }
+    }
 
-// Configure end game
-
-// Check for win (if missed guesses is 5 then lose, if phrase list is = to length then win)
-
-// Log guess calculations
+const phraseArray = getRandomPhraseAsArray(phrases);
+addPhraseToDisplay(phraseArray);
